be8dafba1e64a32c781baa12d559abfc52f66e37
Chelsea <chelsea@tripwiretech.com>
2023-08-13 08:27:36 +1200

    Initial Commit

---
+++ CustomViewModifierTutorial/CustomViewModifierTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifierTutorialApp.swift
+//  CustomViewModifierTutorial
+//
+//  Created by Charles Suddens on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifierTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
