267a83e11887766023ceb575b7e2b2684510ff15
Chelsea <chelsea@tripwiretech.com>
2023-08-15 07:57:41 +1200

    Forgot to add changes

--- CustomViewModifierTutorial/MyBigFont.swift
+++ CustomViewModifierTutorial/MyBigFont.swift
@@ -5,4 +5,18 @@
 //  Created by Charles Suddens on 13/08/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct MyBigFont: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 30))
+            .foregroundColor(Color.blue)
+    }
+}
+
+extension View {
+    public func big() -> some View {
+        modifier(MyBigFont())
+    }
+}
